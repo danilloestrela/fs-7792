f15082f21958259f957e185abf22b8ce4b07f365
Danillo Estrela <danilloestrela@hotmail.com>
2025-02-25 14:39:58 -0300

    Sending changes o/

---
+++ README.md
@@ -0,0 +1,8 @@
+# Setup
+- To run the project:
+1. Go to docker folder: `docker compose up`
+2. Check if database is running on local following the docker-compose.yml config.
+- To run backend:
+1. Use the same version as engines
+2. npm i
+3. npm run dev (for local development)
--- backend/src/http/routes/health-routes.ts
+++ backend/src/http/routes/health-routes.ts
@@ -1,14 +1,13 @@
-import { prisma } from "@/lib/prisma"
 import { FastifyInstance } from "fastify"
 
 export async function healthRoutes(app: FastifyInstance) {
   app.get("/health", async (request, reply) => {
     try {
       // Check database connection
-      await prisma.$queryRaw`SELECT 1`
+      // await prisma.$queryRaw`SELECT 1`
       return { status: "ok", message: "Server is healthy" }
-    } catch (error) {
-      app.log.error("Health check failed", error)
+    } catch (e) {
+      app.log.error("Health check failed", { e })
       reply
         .status(500)
         .send({ status: "error", message: "Server is not healthy" })
